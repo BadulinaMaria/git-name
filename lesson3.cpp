3e82ccb64e1e4372cbb501d15d26616fb28a15ba
Badulina Maria <maskakotik3@gmail.com>
2025-10-13 23:45:43 +0300

    add calculator

---
+++ lesson3.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{/*int a, b, c, d;
+	char dummy;
+	cout << "Enter a/b,c/d" << endl;
+	cin >> a >> dummy >> b >> c >> dummy >> d;
+	float n, f, t;
+	n = static_cast<float>(a)/b;
+	f = static_cast<float>(c)/d;
+	t = n + f;
+	cout << "answer:" << t << endl;
+	return 0; */
+	int a, b, c, d;
+	char dummy;
+	cout << "Enter a/b,c/d" << endl;
+	cin >> a >> dummy >> b >> c >> dummy >> d;
+	float n, f, t;
+	n = static_cast<float>(a) / b;
+	f = static_cast<float>(c) / d;
+	cout << "Which operation? Enter +, -, *, /" << endl;
+	//cin >> '+' >> '-' >> '*' >> '/';
+	char ch;
+	cin >> ch;
+	if (ch == '+') {
+		t = n + f;
+	}
+	else if (ch == '-') {
+		t = n - f;
+	}
+	else if (ch == '*') {
+		t = n * f;
+	}
+	else if (ch == '/') {
+		t = n / f;
+	}
+	cout << "Answer: " << t << endl;
+	return 0;
+}
